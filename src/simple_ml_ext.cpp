ad9f6a654d29ab2f717213dc793ef57738173c64
Slowist-Lee <3230103034@zju.edu.cn>
2025-12-11 21:58:47 +0800

    cpp v4

--- src/simple_ml_ext.cpp
+++ src/simple_ml_ext.cpp
@@ -18,48 +18,47 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      * (and then delete) some helper arrays to store the logits and gradients.
      *
      * Args:
-     *     X (const float *): pointer to X data, of size m*n, stored in row
+     *     X (const float *): posize_ter to X data, of size m*n, stored in row
      *          major (C) format
-     *     y (const unsigned char *): pointer to y data, of size m
-     *     theta (float *): pointer to theta data, of size n*k, stored in row
+     *     y (const unsigned char *): posize_ter to y data, of size m
+     *     theta (float *): posize_ter to theta data, of size n*k, stored in row
      *          major (C) format
      *     m (size_t): number of examples
      *     n (size_t): input dimension
      *     k (size_t): number of classes
      *     lr (float): learning rate / SGD step size
-     *     batch (int): SGD minibatch size
+     *     batch (size_t): SGD minibatch size
      *
      * Returns:
      *     (None)
      */
 
     /// BEGIN YOUR CODE
-    int epochs = m/batch+((m-batch*m/batch>0)?1:0);
-    int row_num;
-    for (int i=0;i<epochs;i++){
-        int x_start=i*n;
-        int num_example=((i+1)*batch>m?m:((i+1)*batch))-i*batch;
+    size_t epochs = m/batch+((m-batch*m/batch>0)?1:0);
+    for (size_t i=0;i<epochs;i++){
+        size_t x_start=i*n;
+        size_t num_example=((i+1)*batch>m?m:((i+1)*batch))-i*batch;
 
         float* data = (float*)malloc(num_example * k * sizeof(float));
 
         float* data_sum = (float*)malloc(num_example * 1 * sizeof(float));
 
 
-        int* Iy = (int*)malloc(num_example * k * sizeof(int));//y:m, Iy:m*k 
+        size_t* Iy = (size_t*)malloc(num_example * k * sizeof(size_t));//y:m, Iy:m*k 
 
         for (size_t i = 0; i < num_example * k; ++i) {
             Iy[i] = 0;
         }
 
-        for(int m_index=0;m_index<num_example;m_index++){
+        for(size_t m_index=0;m_index<num_example;m_index++){
             Iy[m_index*y[m_index]+y[m_index]]=1;
         }
 
-        for(int m_index=0;m_index<num_example;m_index++){
+        for(size_t m_index=0;m_index<num_example;m_index++){
             data_sum[m_index]=0;
-            for(int k_index=0;k_index<k;k_index++){
+            for(size_t k_index=0;k_index<k;k_index++){
                 data[m_index*k+k_index]=0;
-                for(int n_index=0;n_index<n;n_index++){
+                for(size_t n_index=0;n_index<n;n_index++){
                     data[m_index*k+k_index]+=X[x_start+m_index*n+n_index]*theta[n_index*k+k_index];
                 }
                 data[m_index*k+k_index]=std::exp(data[m_index*k+k_index]);//分子
@@ -73,24 +72,23 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
             Z[i] = 0;
         }
 
-        for(int m_index=0;m_index<num_example;m_index++){
-            for(int k_index=0;k_index<k;k_index++){
+        for(size_t m_index=0;m_index<num_example;m_index++){
+      k_indexsize_t      for(size_t k_index=0;k_index<k;k_index++){
                 Z[m_index*k+k_index]=data[m_index*k+k_index]/data_sum[m_index];
             }
         }
 
         float* dt = (float*)malloc(n * k * sizeof(float));
 
-        for(int n_index=0;n_index<n;n_index++){
-            for(int k_index=0;k_index<k;k_index++){
+        for(size_t n_index=0;n_index<n;n_index++){
+            for(size_t k_index=0;k_index<k;k_index++){
                 dt[n_index*k+k_index]=0;
-                for(int m_index=0;m_index<num_example;m_index++){
-                    dt+=X[x_start+m_index*n+n_index]*(Z[m_index*k+k_index]-Iy[m_index*k+k_index])/m;
+                for(size_t m_index=0;m_index<num_example;m_index++){
+                    dt[n_index*k+k_index]+=X[x_start+m_index*n+n_index]*(Z[m_index*k+k_index]-Iy[m_index*k+k_index])/m;
                 }
                 theta[n_index*k+k_index]=theta[n_index*k+k_index]-lr*dt[n_index*k+k_index];
             }
         }
-
     };
     /// END YOUR CODE
 }
@@ -107,7 +105,7 @@ PYBIND11_MODULE(simple_ml_ext, m) {
            py::array_t<unsigned char, py::array::c_style> y,
            py::array_t<float, py::array::c_style> theta,
            float lr,
-           int batch) {
+           size_t batch) {
         softmax_regression_epoch_cpp(
         	static_cast<const float*>(X.request().ptr),
             static_cast<const unsigned char*>(y.request().ptr),
